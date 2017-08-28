ea284a28065ffbd49943abf56400d07aefedf475
Jordan Scales <scalesjordan@gmail.com>
2017-08-27 21:16:00 -0400

    add unfold method to make reducers easier to write

--- src/types.ml
+++ src/types.ml
@@ -2,3 +2,9 @@ type action_type =
   | Reset
   | Increment of int
   | Decrement of int
+
+type state = < count: int > Js.t
+type action = < _type: string; value: action_type > Js.t
+
+let unfold reducer (state: state) (action: action) =
+  reducer state action##value
