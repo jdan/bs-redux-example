284ea792d31c54e89ccb1d82d48a25f3b77cec16
Jordan Scales <scalesjordan@gmail.com>
2017-08-28 14:15:22 -0400

    more refactoring

--- src/reducers.ml
+++ src/reducers.ml
@@ -1,29 +1,33 @@
 open Types;;
 
-let add_todo todos text =
-  [%bs.obj {
-    id = List.length todos;
-    text = text;
-    complete = false
-  }] :: todos
-
-let rec set_completeness todos id complete = match todos with
-  | [] -> []
-  | (todo::tail) ->
-    if todo##id = id then [%bs.obj {
-      id = id;
-      text = todo##text;
-      complete = complete;
-    }] :: tail
-    else todo :: set_completeness tail id complete
+let make_todo id text complete = [%bs.obj {
+  id = id;
+  text = text;
+  complete = complete;
+}]
 
 let object_of_todos todos = [%bs.obj {
   todos = todos
 }]
 
+let add_todo todos text =
+  Array.append
+    todos
+    [|
+      make_todo (Array.length todos) text false
+    |]
+
+let set_completeness todos id complete =
+  Array.map
+    (fun todo ->
+       if id = todo##id
+       then make_todo id todo##text complete
+       else todo
+    )
+    todos
+
 let todos = unfold @@ fun state ->
-  let state_todos = state##todos |> Array.to_list in
   function
-  | Add text -> add_todo state_todos text |> Array.of_list |> object_of_todos
-  | Complete id -> set_completeness state_todos id true |> Array.of_list |> object_of_todos
-  | Uncomplete id -> set_completeness state_todos id false |> Array.of_list |> object_of_todos
+  | Add text -> add_todo state##todos text |> object_of_todos
+  | Complete id -> set_completeness state##todos id true |> object_of_todos
+  | Uncomplete id -> set_completeness state##todos id false |> object_of_todos
