a25f2c9e76ce4d37d8b06772827eef421ee1870f
Jordan Scales <scalesjordan@gmail.com>
2017-08-28 14:32:24 -0400

    add README and hax documentation

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ## bs-redux-example
 
-This is an experiment in writing [Redux](http://redux.js.org/) reducers for a React app in OCaml with [BuckleScript](https://github.com/bucklescript/bucklescript)
+This is an experiment in writing [Redux](http://redux.js.org/) reducers for a React app in OCaml with [BuckleScript](https://github.com/bucklescript/bucklescript).
+
+I'm using [nwb](https://github.com/insin/nwb) to run the React app because it's a very handy project.
 
 ### Run it
 
--- src/index.js
+++ src/index.js
@@ -7,6 +7,27 @@ import App from "./App.js"
 import { todos } from "../lib/js/src/reducers.js"
 import { init } from "../lib/js/src/actions.js"
 
+/**
+ * Hacks to make this work with OCaml :)
+ *
+ * Below are two functions `wrapInit`, which wraps are reducer,
+ * and `tag`, which is used as a middleware.
+ */
+
+/**
+ * Wrap our reducer which is incompatible with the `@@redux/INIT`
+ * magic action.
+ *
+ * Why? Because our reducers work only on our type for `action_type`
+ * (see types.ml), which is defined to be
+ *    Add of text | Complete of int | Uncomplete of int
+ *
+ * Even if we provide a "default" case for our reducer's `match`, there
+ * is no way to get the compiled JS code to handle an arbitrary `action`.
+ *
+ * BuckleScript knows we want an `action_type`, and will `switch` ONLY
+ * for that type! Not a string like `@@redux/INIT`.
+ */
 function wrapInit(reducer) {
   return (state, action) => {
     if (action.type === "@@redux/INIT") {
@@ -16,10 +37,16 @@ function wrapInit(reducer) {
   }
 }
 
+/**
+ * Our action creators and reducers work on `action_type`s, and NOT
+ * json objects.
+ *
+ * Redux only allows us to dispatch objects, so `tag` will create
+ * objects for us! `unfold` (see types.ml) will extract the `action_type`.
+ */
 function tag() {
   return next => action => {
     next({
-      // Should prob get the type from action's tag?
       type: `tagged_${action.tag}`,
       value: action,
     })
